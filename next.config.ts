14acd4453f6bf07bdd5faa60d5a0cefd9e05fb55
waseemkhalid9 <waseem@swiftnine.com>
2026-08-25 12:43:17 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
   },        
     
   webpack(config) {   
-    config.module.rules.push({
+    config.module.rules.push({    
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
