b8b02d384a17245c0695adb5a3f9b1ad0d2fd654
Abbas Ali <abbasalics19@gmail.com>
2026-08-25 18:58:46 +0500

    Fixed the uniersity Pages

--- src/app/(university)/UniversityLayoutClient.tsx
+++ src/app/(university)/UniversityLayoutClient.tsx
@@ -37,7 +37,8 @@ export default function UniversityLayoutClient({
       <AppHeader />
       <div className="flex flex-1 overflow-hidden">
         <AppSidebar hasHeader={true} />
-        <div className="flex flex-col flex-1 min-w-0 ml-80 overflow-hidden">
+        {/* Offset by the sidebar: 72px icon rail (plus its 8px of margins) + 264px panel. */}
+        <div className="flex flex-col flex-1 min-w-0 ml-[336px] overflow-hidden">
           <main className="flex-1 overflow-y-auto [&::-webkit-scrollbar]:w-1.5 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-200 dark:[&::-webkit-scrollbar-thumb]:bg-gray-800 border border-t border-r border-b mr-2 mb-2 rounded-tr-lg rounded-br-lg border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-900">
             <div className="max-w-[1400px] mx-auto">
             {children}
